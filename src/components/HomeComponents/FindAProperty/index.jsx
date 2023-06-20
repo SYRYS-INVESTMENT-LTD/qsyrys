ef1a4d3a0488ab2ffd894cb77d6bde8b9d4bbdbc
george-hub331 <idiaghegeorge9@gmail.com>
2023-06-20 10:18:06 +0100

    update: reduce min width

--- src/components/HomeComponents/FindAProperty/index.jsx
+++ src/components/HomeComponents/FindAProperty/index.jsx
@@ -49,12 +49,7 @@ const FindAProperty = () => {
                         spaceBetween={30}
                         slidesPerView={4}
                         breakpoints={{
-                          992: {
-                            slidesPerView: 2,
-                          },
-                          480: {
-                            slidesPerView: 1,
-                          }
+                          
                         }}
                         pagination={{ clickable: true }}
                         navigation={{
@@ -65,6 +60,204 @@ const FindAProperty = () => {
                         className="property-content owl-carousel manual owl-nav-top-right owl-loaded owl-drag"
                         data-callback="owl_callback"
                       >
+                        <SwiperSlide className="item">
+                          <div className="ere-item-wrap property-item ere-property-featured">
+                            <div className="property-inner">
+                              <div className="property-image">
+                                <img
+                                  decoding="async"
+                                  loading="lazy"
+                                  width="330"
+                                  height="180"
+                                  src="https://www.qsyrys.com/wp-content/uploads/2022/07/1-1-5-330x180.jpg"
+                                  alt="1+1 A CITY"
+                                  title="1+1 A CITY"
+                                />
+                                <div className="property-action block-center">
+                                  <div className="block-center-inner">
+                                    <div
+                                      className="property-view-gallery-wrap"
+                                      data-toggle="tooltip"
+                                      title=""
+                                      data-original-title="(10) Photos"
+                                    >
+                                      <a
+                                        data-property-id="8381"
+                                        href={"#"}
+                                        className="property-view-gallery"
+                                      >
+                                        <i className="fa fa-camera"></i>
+                                      </a>
+                                    </div>
+                                    <a
+                                      href={"#"}
+                                      className="property-favorite"
+                                      data-property-id="8381"
+                                      data-toggle="tooltip"
+                                      title=""
+                                      data-title-not-favorite="Add to Favorite"
+                                      data-title-favorited="It is my favorite"
+                                      data-icon-not-favorite="fa fa-star-o"
+                                      data-icon-favorited="fa fa-star"
+                                      data-original-title="Add to Favorite"
+                                    >
+                                      <i className="fa fa-star-o"></i>
+                                    </a>
+                                    <a
+                                      className="compare-property"
+                                      href={"#"}
+                                      data-property-id="8381"
+                                      data-toggle="tooltip"
+                                      title=""
+                                      data-original-title="Compare"
+                                    >
+                                      <i className="fa fa-plus"></i>
+                                    </a>{" "}
+                                  </div>
+                                  <a
+                                    className="property-link"
+                                    href="https://www.qsyrys.com/property/11-a-city/"
+                                    title="1+1 A CITY"
+                                  ></a>
+                                </div>
+                                <div className="property-label property-featured">
+                                  <p className="label-item">
+                                    <span className="property-label-bg">
+                                      Featured{" "}
+                                      <span className="property-arrow"></span>
+                                    </span>
+                                  </p>
+                                </div>
+                                <div className="property-status">
+                                  <p className="status-item">
+                                    <span
+                                      className="property-status-bg"
+                                      style={{ backgroundColor: "#dd3333" }}
+                                    >
+                                      For Sale{" "}
+                                      <span
+                                        className="property-arrow"
+                                        style={{
+                                          borderLeftColor: "#dd3333",
+                                          borderRightColor: "#dd3333",
+                                        }}
+                                      ></span>
+                                    </span>
+                                  </p>
+                                </div>
+                              </div>
+                              <div className="property-item-content">
+                                <div className="property-heading">
+                                  <h2 className="property-title">
+                                    <a
+                                      href="https://www.qsyrys.com/property/11-a-city/"
+                                      title="1+1 A CITY"
+                                    >
+                                      1+1 A CITY
+                                    </a>
+                                  </h2>
+                                  <div className="property-price">
+                                    <span>97,000£ </span>
+                                  </div>
+                                </div>
+                                <div
+                                  className="property-location"
+                                  title="SYRYS INVESTMENT LTD"
+                                >
+                                  <i className="fa fa-map-marker"></i>
+                                  <a
+                                    target="_blank"
+                                    href="http://maps.google.com/?q=SYRYS%20INVESTMENT%20LTD"
+                                  >
+                                    <span>SYRYS INVESTMENT LTD</span>
+                                  </a>
+                                </div>
+                                <div className="property-element-inline">
+                                  <div className="property-type-list">
+                                    <i className="fa fa-tag"></i>
+                                    <a
+                                      href="https://www.qsyrys.com/property-type/residential-complex/"
+                                      title="Residential Complex"
+                                    >
+                                      <span>Residential Complex </span>
+                                    </a>
+                                  </div>
+                                  <div className="property-agent">
+                                    <a
+                                      href="https://www.qsyrys.com/author/syrysgmail-com/"
+                                      title="SYRYS GROUP"
+                                    >
+                                      {" "}
+                                      <i className="fa fa-user"></i>
+                                      <span>SYRYS GROUP</span>
+                                    </a>{" "}
+                                  </div>
+                                  <div className="property-date">
+                                    <i className="fa fa-calendar"></i>
+                                    11 months ago{" "}
+                                  </div>
+                                </div>
+                                <div className="property-excerpt">
+                                  <p>
+                                    A CITY What makes the A City unique besides
+                                    urban life is the futures that anticipate
+                                    the needs, quality, respectable and reliable
+                                    projects that create a difference, sensitive
+                                    to the environment, with high brand value
+                                    and walking distance to the bazaar, shopping
+                                    malls, markets, schools not forgetting
+                                    nature and amazing sea, mountain and city
+                                    views. […]
+                                  </p>
+                                </div>
+                                <div className="property-info">
+                                  <div className="property-info-inner">
+                                    <div className="property-area">
+                                      <div
+                                        className="property-area-inner property-info-item-tooltip"
+                                        data-toggle="tooltip"
+                                        title=""
+                                        data-original-title="Size"
+                                      >
+                                        <span className="fa fa-arrows"></span>
+                                        <span className="property-info-value">
+                                          61.6 m<sup>2</sup>{" "}
+                                        </span>
+                                      </div>
+                                    </div>
+                                    <div className="property-bedrooms">
+                                      <div
+                                        className="property-bedrooms-inner property-info-item-tooltip"
+                                        data-toggle="tooltip"
+                                        title=""
+                                        data-original-title="1 Bedroom"
+                                      >
+                                        <span className="fa fa-hotel"></span>
+                                        <span className="property-info-value">
+                                          1
+                                        </span>
+                                      </div>
+                                    </div>
+                                    <div className="property-bathrooms">
+                                      <div
+                                        className="property-bathrooms-inner property-info-item-tooltip"
+                                        data-toggle="tooltip"
+                                        title=""
+                                        data-original-title="1 Bathroom"
+                                      >
+                                        <span className="fa fa-bath"></span>
+                                        <span className="property-info-value">
+                                          1
+                                        </span>
+                                      </div>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+                        </SwiperSlide>
+
                         <SwiperSlide className="item">
                           <div className="ere-item-wrap property-item ere-property-featured">
                             <div className="property-inner">
